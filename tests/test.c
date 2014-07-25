858dd86d3dbb11cb7b6ea8e85574ff9927e30513
tsm <up201002596@alunos.dcc.fc.up.pt>
2014-07-25 22:48:54 +0100

    Structured repository, adding folders

---
+++ tests/test.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main() { // comment 1
+    int i = 0; 
+    int a = 0;/*
+ Block Comment 1
+              */int b = 0;
+    int c = 0;
+    printf("%d", i); // comment 2
+
+    int d = 0; /* block */
+    int e = 0;
+
+    int f;/*
+
+           */
+    int g;
+    /*
+
+     */ int h;
+
+    /*
+      Block Comment 2
+    */
+}
